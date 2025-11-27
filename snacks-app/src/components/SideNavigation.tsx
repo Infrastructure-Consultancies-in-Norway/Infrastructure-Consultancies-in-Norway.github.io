77177a26aac28c1bde46f0710f2516e3862492ca
Cobse <Fredrik.jacobsen85@gmail.com>
2025-11-27 15:50:50 +0100

    fixed navigation

---
+++ snacks-app/src/components/SideNavigation.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type SideNavigationProps = {
+  scrollToSection: (id: string) => void;
+};
+
+const navItems = [
+  { id: 'snacks-main', label: 'Hjem' },
+  { id: 'standardisering-del-1', label: 'Standardisering 1' },
+  { id: 'snacks-strukturen', label: 'Strukturen' },
+  { id: 'standardisering-del-2', label: 'Standardisering 2' },
+  { id: 'historie', label: 'Historie' },
+  { id: 'last-ned', label: 'Last ned' },
+  { id: 'begrepsforklaring', label: 'Begrepsforklaring' },
+  { id: 'kontakt', label: 'Kontakt' },
+];
+
+const SideNavigation: React.FC<SideNavigationProps> = ({ scrollToSection }) => {
+  return (
+    <nav className="side-navigation sticky-top top-50 translate-middle-y">
+      <ul className="nav flex-column">
+        {navItems.map((item) => (
+          <li className="nav-item" key={item.id}>
+            <button
+              type="button"
+              className="nav-link text-dark side-nav-link"
+              onClick={() => scrollToSection(item.id)}
+            >
+              {item.label}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavigation;
--- snacks-app/src/pages/OnePage.tsx
+++ snacks-app/src/pages/OnePage.tsx
@@ -7,6 +7,7 @@ import Historie from '../components/Historie';
 import LastNed from '../components/LastNed';
 import Begrepsforklaring from '../components/Begrepsforklaring';
 import Kontakt from '../components/Kontakt';
+import SideNavigation from '../components/SideNavigation';
 
 const OnePage: React.FC = () => {
   const scrollToSection = (id: string) => {
@@ -17,9 +18,9 @@ const OnePage: React.FC = () => {
   };
 
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="row">
-        <div className="col-md-10">
+        <div className="col-12 col-md-8 col-lg-9 col-xl-10">
           <SnacksMain />
           <StandardiseringDel1 />
           <SnacksStrukturen />
@@ -29,35 +30,8 @@ const OnePage: React.FC = () => {
           <Begrepsforklaring />
           <Kontakt />
         </div>
-        <div className="col-md-2 d-none d-md-block">
-          <nav className="position-fixed top-50 translate-middle-y end-0 pe-3">
-            <ul className="nav flex-column text-end">
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('snacks-main')}>Hjem</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('standardisering-del-1')}>Standardisering 1</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('snacks-strukturen')}>Strukturen</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('standardisering-del-2')}>Standardisering 2</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('historie')}>Historie</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('last-ned')}>Last ned</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('begrepsforklaring')}>Begrepsforklaring</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-link nav-link text-dark" onClick={() => scrollToSection('kontakt')}>Kontakt</button>
-              </li>
-            </ul>
-          </nav>
+        <div className="col-12 col-md-4 col-lg-3 col-xl-2 d-none d-md-block">
+          <SideNavigation scrollToSection={scrollToSection} />
         </div>
       </div>
     </div>
